6bb792492b4abe55e5793041a3bcfae978f939f5
AndresWnj <andres.wnj@gmail.com>
2022-08-26 09:05:07 -0500

    fcm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDMMfK233HBhDFs_kjGqSGx_Bag2q0yUow",
+    authDomain: "casadelrey-e6cf6.firebaseapp.com",
+    databaseURL: "https://casadelrey-e6cf6.firebaseio.com",
+    projectId: "casadelrey-e6cf6",
+    storageBucket: "casadelrey-e6cf6.appspot.com",
+    messagingSenderId: "966101535658",
+    appId: "1:966101535658:web:395eecdcbcaa8f8396f9d3",
+    measurementId: "G-L0153YQVH8",
+    vapidKey: "BDnWmCJgnBQwn5nFYttg-9fqnU9niu5irQJ9I19hQalXkgXERmqrkRVkCOp0tAsAl9QXEayKxEUy2501gLWK-o4"
+  },
 };
