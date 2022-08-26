20a6d7a971e0447100fb913c261d5652827b8074
AndresWnj <andres.wnj@gmail.com>
2022-08-26 11:09:25 -0500

    asdajdnad

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,8 +2,12 @@
 //importScripts("https://www.gstatic.com/firebasejs/9.1.3/firebase-messaging-compat.js");
 
 
-importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.0.2/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/7.14.6/firebase-messaging.js');
+
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+
 
 
 firebase.initializeApp({
@@ -18,71 +22,82 @@ firebase.initializeApp({
   });
 const messaging = firebase.messaging();
 
-messaging.setBackgroundMessageHandler(function (payload) {
-    console.log(payload);
+messaging.onBackgroundMessage(function(payload) {
+    console.log("ALGO LLEGO ", payload);
+ 
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+ 
+    self.registration.showNotification(notificationTitle, notificationOptions);
+  });
 
-    var notificationTitle = payload.data.Titulo;
+// messaging.setBackgroundMessageHandler(function (payload) {
+//     console.log(payload);
 
-    var listener = new BroadcastChannel('listener');
-    listener.postMessage(payload.data);
+//     var notificationTitle = payload.data.Titulo;
+
+//     var listener = new BroadcastChannel('listener');
+//     listener.postMessage(payload.data);
     
-    if (!payload.data.RefreshCentroDespacho || payload.data.RefreshCentroDespacho === "false") {
+//     if (!payload.data.RefreshCentroDespacho || payload.data.RefreshCentroDespacho === "false") {
 
-        //listener.postMessage("1");
+//         //listener.postMessage("1");
 
-        console.log("SI CREO PUSH BACKGROUND");
+//         console.log("SI CREO PUSH BACKGROUND");
 
-        var notificationOptions = {
-            body: payload.data.Descripcion,
-            icon: '/Recursos/images/flash.png',
-            data: { url: payload.data.Url2, id: payload.data.ItemId },
-            actions: [{ action: "open_url", title: "Abrir Centro Control" }]
-        };
+//         var notificationOptions = {
+//             body: payload.data.Descripcion,
+//             icon: '/Recursos/images/flash.png',
+//             data: { url: payload.data.Url2, id: payload.data.ItemId },
+//             actions: [{ action: "open_url", title: "Abrir Centro Control" }]
+//         };
 
-        var a = self.registration.showNotification(notificationTitle, notificationOptions);
-        return a;
-    } else {
-        console.log("NO CREO PUSH BACKGROUND");
-        //listener.postMessage("0");
-    }
+//         var a = self.registration.showNotification(notificationTitle, notificationOptions);
+//         return a;
+//     } else {
+//         console.log("NO CREO PUSH BACKGROUND");
+//         //listener.postMessage("0");
+//     }
 
-    //return a;
-});
+//     //return a;
+// });
 
-self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+// self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+// self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 
-self.addEventListener('notificationclick', function (event) {
+// self.addEventListener('notificationclick', function (event) {
 
-    switch (event.action) {
-        case 'open_url':
-            var url = event.notification.data.url;
-            console.log("open_url", url);
-            //clients.openWindow(url); //which we got from above
+//     switch (event.action) {
+//         case 'open_url':
+//             var url = event.notification.data.url;
+//             console.log("open_url", url);
+//             //clients.openWindow(url); //which we got from above
 
-            event.notification.close(); // Android needs explicit close.
-            event.waitUntil(
+//             event.notification.close(); // Android needs explicit close.
+//             event.waitUntil(
 
-                clients.matchAll({ includeUncontrolled: true, type: 'window' }).then(windowClients => {
-                    console.log("cuales clientes hay");
-                    console.log(windowClients);
-                    // Check if there is already a window/tab open with the target URL
-                    for (var i = 0; i < windowClients.length; i++) {
-                        var client = windowClients[i];
-                        // If so, just focus it.
-                        if (client.url === url && 'focus' in client) {
-                            return client.focus();
-                        }
-                    }
-                    // If not, then open the target URL in a new window/tab.
-                    if (clients.openWindow) {
-                        return clients.openWindow(url);
-                    }
-                })
-            );
+//                 clients.matchAll({ includeUncontrolled: true, type: 'window' }).then(windowClients => {
+//                     console.log("cuales clientes hay");
+//                     console.log(windowClients);
+//                     // Check if there is already a window/tab open with the target URL
+//                     for (var i = 0; i < windowClients.length; i++) {
+//                         var client = windowClients[i];
+//                         // If so, just focus it.
+//                         if (client.url === url && 'focus' in client) {
+//                             return client.focus();
+//                         }
+//                     }
+//                     // If not, then open the target URL in a new window/tab.
+//                     if (clients.openWindow) {
+//                         return clients.openWindow(url);
+//                     }
+//                 })
+//             );
 
-            break;
-    }
-}
-    , false);
+//             break;
+//     }
+// }
+//     , false);
